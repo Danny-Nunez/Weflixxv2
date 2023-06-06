72897f7f63c8f5d797d117f891456f01b307e141
Danny Nunez <dnunez22@gmail.com>
2023-06-05 22:24:21 -0400

    updates

--- components/Row.tsx
+++ components/Row.tsx
@@ -47,7 +47,7 @@ function Row({ title, movies }: Props) {
         >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
-            {movie.title}
+            
           ))}
         </div>
 
