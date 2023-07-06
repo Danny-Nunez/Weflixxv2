802d92f440772d2aab4de6bcbb81a4211ad8e389
Danny Nunez <dnunez22@gmail.com>
2023-07-06 10:08:22 -0400

    updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import { BellIcon, SearchIcon, UserIcon, ArrowLeftIcon, PencilIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
@@ -9,18 +9,15 @@ import { useRouter } from 'next/router'
 import SearchForm from './SearchForm';
 import ProfileImage from './ProfileImage';
 
-
-// import { ChevronDownIcon } from '@heroicons/react/20/solid'
-
 interface ProfileImageProps {
   photoURL: string | null | undefined;
 }
-
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
   const { user, logout } = useAuth()
   const router = useRouter();
+  const [isShowing, setIsShowing] = useState(false)
   const [query, setQuery] = useState("");
 
   useEffect(() => {
@@ -50,7 +47,6 @@ function Header() {
       query: { q: query },
     });
   };
-  
 
   return (
     <header className={`${isScrolled && 'bg-[#000]'}`}>
@@ -65,86 +61,105 @@ function Header() {
         <BasicMenu />
 
         <ul className="hidden space-x-4 md:flex">
-        <Link href="/">
+          <Link href="/">
             <li className="headerLink menuShadow">Home</li>
           </Link>
           <Link href="/account">
-          <li className="headerLink menuShadow">My Account</li>
+            <li className="headerLink menuShadow">My Account</li>
           </Link>
-          {/* <Link href="/">
-          <li className="headerLink menuShadow">Movies</li>
-        </Link> */}
-        <Link href="/news">
-          <li className="headerLink menuShadow">News</li>
+          <Link href="/news">
+            <li className="headerLink menuShadow">News</li>
           </Link>
           <Link href="/mylist">
-          <li className="headerLink menuShadow">My List</li>
+            <li className="headerLink menuShadow">My List</li>
           </Link>
         </ul>
       </div>
 
-      <div className="flex text-sm font-light">
-      {/* <Link href="/searchmovies"><SearchIcon className="hidden h-6 w-6 sm:inline cursor-pointer" /></Link> */}
+      <div onMouseEnter={() => setIsShowing(true)}
+            onMouseLeave={() => setIsShowing(false)} 
+            className="flex text-sm font-light triangle">
+        <div className="hidden md:inline">
+          <SearchForm query={query} setQuery={setQuery} onSubmit={handleSearch} />
+        </div>
 
-      <div className=" hidden md:inline  "><SearchForm query={query} setQuery={setQuery} onSubmit={handleSearch} /></div>
-
-
-      <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md  px-1 py-2 pb-1 pt-1 text-sm  ring-inset ring-gray-300 hover:text-gray-200">
-       {/* <span className="pt-1 pl-1 menuShadow">Adult</span> */}
-       
-    
-         
-       <ProfileImage photoURL={user?.photoURL || undefined} />
-      
-
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-1 w-26 origin-top-right border-1 rounded-md  ring-0 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-           
-            <Menu.Item>
-              {({ active }) => (
-                <a onClick={logout}
-                  href="#"
-                  className={classNames(
-                    active ? 'text-white-300' : 'text-gray-300',
-                    'block px-0 py-0 text-sm mr-1'
-                  )}
-                >
-                  Logout
-                </a>
-              )}
-            </Menu.Item>
-           
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button 
             
+            className="  inline-flex w-full justify-center gap-x-1.5 rounded-md px-1 py-2 pb-1 pt-1 text-sm ring-inset ring-gray-300 hover:text-gray-200 focus:outline-none">
+              
+              <ProfileImage photoURL={user?.photoURL || undefined} />
+              <svg className="mt-4 " xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="white" viewBox="0 0 24 24"><path d="M18.5,15.5l-6-7l-6,7H18.5z"/></svg>
+             
+            </Menu.Button>
           </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-        {/* <p className="hidden lg:inline"><Link href="/">Adult</Link></p> */}
-        {/* <BellIcon className="h-6 w-6" /> */}
-        {/* <Link href="/account">
-          <img
-            src="/adulticon.jpg"
-            alt=""
-            className="cursor-pointer rounded"
-          />
-        </Link> */}
+
+          <Transition
+            show={isShowing}
+            // onMouseEnter={() => setIsShowing(true)}
+            // onMouseLeave={() => setIsShowing(false)}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute bg-black right-0 z-10 mt-1 w-36 origin-top-right border-1 rounded-md ring-0 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item>
+                  <Link href="/account">
+                    <a className="text-gray-300 block px-0 py-2 text-sm mr-1 flex-row flex hover:underline">
+                      
+                    <img
+                    src="/user.png"
+                    width="14px"
+                    height="14px"
+                    className="cursor-pointer object-contain ml-3 mr-2"
+        />Account
+                    </a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item>
+                  <Link href="/account">
+                    <a className="text-gray-300 block px-0 py-2 text-sm mr-1 flex-row flex hover:underline">
+                    <img
+                    src="/edit.png"
+                    width="14px"
+                    height="14px"
+                    className="cursor-pointer object-contain ml-3 mr-2"
+        />
+                      Edit Profile
+                    </a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item >
+                 
+                  <a
+                    onClick={logout}
+                    href="#"
+                    className="border-t border-gray-700 text-gray-300 block px-0 py-2 mt-2 text-sm mr-1 flex-row flex hover:underline"
+                  >
+                    <img
+                    src="/logout.png"
+                    width="15px"
+                    height="15px"
+                    className="cursor-pointer object-contain ml-3 mr-2"
+        />
+                    Logout
+                  </a>
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -10,20 +10,12 @@ interface SearchFormProps {
     return (
     <form className="max-w-sm px-6 mt-2" onSubmit={onSubmit}>
       <div className="relative">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+      <img
+                    src="/search.png"
+                    width="20px"
+                    height="20px"
+                    className="object-contain left-3 top-2 absolute "
+        />
         <input
           type="text"
           value={query}
