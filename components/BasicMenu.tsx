2688165bbae9c34809d44e861f62110b28da307b
Danny Nunez <dnunez22@gmail.com>
2023-08-04 11:04:02 -0400

    updates

--- components/BasicMenu.tsx
+++ components/BasicMenu.tsx
@@ -60,14 +60,15 @@ export default function BasicMenu() {
         <MenuItem onClick={handleClose}><Link href="/account">My Account</Link></MenuItem>
         <MenuItem onClick={handleGenreClick}>Genre</MenuItem>
         <Collapse in={isGenreOpen}>
-          <div className="grid grid-cols-2 gap-0 pl-2">
+          <div className="w-72 grid grid-cols-2 gap-0 ">
             <MenuItem>
-              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+              <div className="hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
                 <img src="/action.png" width="16px" height="16px"
                     className="cursor-pointer object-contain ml-1 mr-2"/>
                 <Link href="/action">Action</Link>
               </div>
             </MenuItem>
+            
             <MenuItem>
               <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
                 <img src="/crime.png" width="16px" height="16px"
