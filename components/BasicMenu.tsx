9dcf24d8c61d517d65504c9c38293881bd91e2b1
Danny Nunez <dnunez22@gmail.com>
2023-08-04 10:45:29 -0400

    updates

--- components/BasicMenu.tsx
+++ components/BasicMenu.tsx
@@ -1,24 +1,29 @@
 import Button from '@mui/material/Button'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
+import Collapse from '@mui/material/Collapse'
 import { useState } from 'react'
 import Link from 'next/link'
-import SearchFormMobile from './SearchFormMobile'
 import { useRouter } from 'next/router'
 import { SearchIcon } from '@heroicons/react/solid'
 
 export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [isGenreOpen, setGenreOpen] = useState(false);
+  const open = Boolean(anchorEl);
   const router = useRouter();
   const [query, setQuery] = useState("");
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
+    setAnchorEl(event.currentTarget);
   }
 
   const handleClose = () => {
-    setAnchorEl(null)
+    setAnchorEl(null);
+  }
+
+  const handleGenreClick = () => {
+    setGenreOpen(!isGenreOpen);
   }
 
   const handleSearch = (event: React.FormEvent) => {
@@ -53,13 +58,59 @@ export default function BasicMenu() {
       >
         <MenuItem onClick={handleClose}><Link href="/">Home</Link></MenuItem>
         <MenuItem onClick={handleClose}><Link href="/account">My Account</Link></MenuItem>
-        <MenuItem onClick={handleClose}><Link href="/">Movies</Link></MenuItem>
+        <MenuItem onClick={handleGenreClick}>Genre</MenuItem>
+        <Collapse in={isGenreOpen}>
+          <div className="grid grid-cols-2 gap-0 pl-2">
+            <MenuItem>
+              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+                <img src="/action.png" width="16px" height="16px"
+                    className="cursor-pointer object-contain ml-1 mr-2"/>
+                <Link href="/action">Action</Link>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+                <img src="/crime.png" width="16px" height="16px"
+                    className="cursor-pointer object-contain ml-1 mr-2"/>
+                <Link href="/crime">Crime</Link>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+                <img src="/adventure.png" width="16px" height="16px"
+                    className="cursor-pointer object-contain ml-1 mr-2"/>
+                <Link href="/adventure">Adventure</Link>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+                <img src="/fantasy.png" width="16px" height="16px"
+                    className="cursor-pointer object-contain ml-1 mr-2"/>
+                <Link href="/fantasy">Fantasy</Link>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+                <img src="/drama.png" width="16px" height="16px"
+                    className="cursor-pointer object-contain ml-1 mr-2"/>
+                <Link href="/drama">Drama</Link>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div className=" hover:underline cursor-pointer flex inline text-xs font-light text-gray-300">
+                <img src="/horror.png" width="16px" height="16px"
+                    className="cursor-pointer object-contain ml-1 mr-2"/>
+                <Link href="/horror">Horror</Link>
+              </div>
+            </MenuItem>
+          </div>
+        </Collapse>
         <MenuItem onClick={handleClose}><Link href="/news">News</Link></MenuItem>
         <MenuItem onClick={handleClose}><Link href="/mylist">My List</Link></MenuItem>
-        {/* <MenuItem >
-        <SearchFormMobile query={query} setQuery={setQuery} onSubmit={handleSearch} />
-        </MenuItem> */}
       </Menu>
     </div>
-  )
+  );
 }
+
+
+
